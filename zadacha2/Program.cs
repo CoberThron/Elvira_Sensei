c0fadc6f2919c8b20064eb012980f704508142ed
User <kobrik46@gmail.com>
2023-05-22 21:12:07 +0300

    задача 2 решена

---
+++ zadacha2/Program.cs
@@ -0,0 +1,24 @@
+﻿int FirstNumber;
+int SecondNumber;
+
+Console.Write("введите число 1: ");
+FirstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите число 2: ");
+SecondNumber = int.Parse(Console.ReadLine()!);
+
+if (FirstNumber > SecondNumber)
+{
+    Console.Write("max = ");
+    Console.WriteLine(FirstNumber);
+    Console.Write("min = ");
+    Console.WriteLine(SecondNumber);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(SecondNumber);
+    Console.Write("min = ");
+    Console.WriteLine(FirstNumber);
+}
+
