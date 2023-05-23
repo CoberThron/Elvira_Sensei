e69d1548c7d32447719e3cff4b9cc2bcf32ae33b
User <kobrik46@gmail.com>
2023-05-23 09:33:44 +0300

    zadacha 8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);    // Считываем число N
+
+Console.WriteLine($"Чётные числа от 1 до {n}: ");
+
+for (int i = 2; i <= n; i += 2)             // Используем цикл для перебора чётных чисел
+{
+    Console.WriteLine(i);
+}    
