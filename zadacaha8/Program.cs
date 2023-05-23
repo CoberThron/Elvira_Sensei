930ab626f54890f5c752e12ea58d96f39707a558
User <kobrik46@gmail.com>
2023-05-23 09:57:37 +0300

    zadacha 8

---
+++ zadacaha8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int Number = int.Parse(Console.ReadLine()!);    
+Console.WriteLine($"Чётные числа от 1 до {Number}:");
+int Count = 2;
+while (Count <= Number)             
+{
+ Console.WriteLine(Count);
+ Count += 2;
+}
+
