72cb087bed0fd1b7bd7972fdd75488de746408d4
User <kobrik46@gmail.com>
2023-05-23 09:06:13 +0300

    переименовал папку

---
+++ zadacha4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите количество чисел:");
+        int n = int.Parse(Console.ReadLine()!);
+        int[] Numbers = new int[n];
+        int i = 0;
+        while(i < n)
+        {
+            Console.WriteLine($"Введите {i + 1}-е число:");
+            Numbers[i] = int.Parse(Console.ReadLine()!);
+            i++;
+        }
+        
+
+        int MaxNumberIndex = 0;
+        int Index = 0;
+
+        while (Index < n)
+        {
+            if (Numbers[Index] > Numbers[MaxNumberIndex])
+            {
+                MaxNumberIndex = Index;
+            }
+            Index++;
+        }
+
+        Console.WriteLine($"Максимальное число: {Numbers[MaxNumberIndex]}");
+   
+
+
